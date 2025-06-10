c2e1e1bbe2c18e7e0aa5d31d6a5f6c2becb100b4
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-06-10 22:37:07 +0700

    init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { DynamodbModule } from './modules/dynamodb/dynamodb.module';
+import { ChatGroupModule } from './modules/chat-group/chat-group.module';
+import { MessagesModule } from './modules/messages/messages.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import { ContactModule } from './modules/contact/contact.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DynamodbModule, ChatGroupModule, MessagesModule, NotificationModule, ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
