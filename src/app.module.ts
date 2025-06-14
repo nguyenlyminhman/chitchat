890fe555f6c37b9287951afa858221c94233ffbf
nguyenlyminhman <nguyenlyminhman@gmail.com>
2025-06-14 10:26:42 +0700

    add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,22 @@ import { ChatGroupModule } from './modules/chat-group/chat-group.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { NotificationModule } from './modules/notification/notification.module';
 import { ContactModule } from './modules/contact/contact.module';
+import { ShareModule } from './modules/share/share.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, DynamodbModule, ChatGroupModule, MessagesModule, NotificationModule, ContactModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }),
+    UserModule,
+    DynamodbModule,
+    ChatGroupModule,
+    MessagesModule,
+    NotificationModule,
+    ContactModule,
+    ShareModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
